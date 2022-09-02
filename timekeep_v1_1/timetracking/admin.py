7b5df1db8cbf592903d004ba27c2abbcb8aafe86
AlanQuaid <aljoshin.anton@gmail.com>
2022-09-02 23:44:28 +0300

    Some CSS

--- timekeep_v1_1/timetracking/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Manager, Employee, Schedule, UserProfile
-
-# Register your models here.
-admin.site.register(Manager)
-admin.site.register(Employee)
-admin.site.register(Schedule)
-admin.site.register(UserProfile)
