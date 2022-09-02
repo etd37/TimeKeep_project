4fea51914f9da9754a1e01a91dfeb582bb1a2637
AlanQuaid <aljoshin.anton@gmail.com>
2022-09-02 23:24:31 +0300

    Some CSS

---
+++ timekeep_v1_1/project/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+from team.models import Team
+
+
+# Create your models here.
+
+class Project(models.Model):
+    team = models.ForeignKey(Team, related_name='projects', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+
+    created_by = models.ForeignKey(User, related_name='projects', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+    def registered_time(self):
+        return sum(entry.minutes for entry in self.entries.all())
+
+
+class Entry(models.Model):
+    team = models.ForeignKey(Team, related_name='entries', on_delete=models.CASCADE, blank=True, null=True)
+    project = models.ForeignKey(Project, related_name='entries', on_delete=models.CASCADE, blank=True, null=True)
+    minutes = models.IntegerField(default=0)
+    is_tracked = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, related_name='entries', on_delete=models.CASCADE)
+    created_at = models.DateTimeField()
+
+    class Meta:
+        ordering = ['-created_at']
+
+    # def __str__(self):
+    #     return '%' % self.created_at
