64c263f811b5561be29b58adf543c2511e9ecf5b
AlanQuaid <111380124+AlanQuaid@users.noreply.github.com>
2022-09-04 04:46:03 +0300

    Delete timekeep_v1_1 directory

--- timekeep_v1_1/summary/templatetags/format_min.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-from django import template
-
-def format_minutes(value):
-    return '{:2d}h {:2d}m'.format(*divmod(value, 60))
-
-register = template.Library()
-register.filter('format_minutes', format_minutes)
