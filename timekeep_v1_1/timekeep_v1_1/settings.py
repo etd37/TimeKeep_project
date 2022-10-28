798e715dfb3d66f4e97979126db66a169cc2f247
ivbojar <ivbojar@gmail.com>
2022-10-29 00:02:46 +0300

    static check

--- timekeep_v1_1/timekeep_v1_1/settings.py
+++ timekeep_v1_1/timekeep_v1_1/settings.py
@@ -139,8 +139,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [Path(BASE_DIR).joinpath('static')]
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = [Path(BASE_DIR).joinpath('static')]
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
